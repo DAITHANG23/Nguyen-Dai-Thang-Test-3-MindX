343413565dc00d34863ba12d6131b2c18520ddb5
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-08-07 02:34:19 +0700

    update

--- project-test-2/db.js
+++ project-test-2/db.js
@@ -1,31 +1,43 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
+
 const uri =
-  "mongodb+srv://thang:hfZSmH8hkqlQK4fC@cluster0.wgrezy8.mongodb.net/?retryWrites=true&w=majority";
-
+  "mongodb+srv://thang:123@cluster0.wgrezy8.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
+let orderCollection, inventoryCollection, userCollection; // Declaring the collections here to make them accessible outside the connectToDb function
 
-async function run(expressApp) {
+async function connectToDb() {
   try {
-    await client.connect(() => {
-      const database = client.db("your_db_name");
-      db.inventories = database.collection("inventories");
-      db.orders = database.collection("orders");
-      db.users = database.collection("users");
-    });
+    await client.connect();
+    console.log("Connected to MongoDB Atlas!");
 
-    const database = await client.db("project");
-    const inventoryCollection = database.collection("Inventory");
-    const item = inventoryCollection.findOne("_id");
-    console.log(item);
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-
-    expressApp.listen("3000", async () => {
-      console.log(`Server is listening on port 3000`);
-    });
-  } finally {
-    await client.close();
+    // Add your database operations here
+    const db = client.db("project");
+    orderCollection = db.collection("Order");
+    inventoryCollection = db.collection("Inventory");
+    userCollection = db.collection("Users");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
   }
 }
-run().catch(console.dir);
+
+async function getInventories(lowQuantity) {
+  try {
+    let query = {};
+    if (lowQuantity === "true") {
+      query = { instock: { $lt: 100 } };
+    }
+    console.log("Query used to fetch products:", query);
+    const inventories = await inventoryCollection.find(query).toArray();
+    console.log("Fetched products:", inventories);
+    return inventories;
+  } catch (err) {
+    console.error("Error fetching inventories:", err);
+    throw err;
+  }
+}
+
+// Exporting the collections and the connectToDb function
+module.exports = {
+  connectToDb,
+  getInventories,
+};
